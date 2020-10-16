8070cadcf77771311e0a76dd9028f9406b56bed8
Gdubol <pbraque@mailoo.org>
2020-10-16 08:46:25 +0200

    ajout cholesky

--- TP2.py
+++ TP2.py
@@ -22,20 +22,54 @@ def qr_gen(n,A):
 #Exercice 2
 def resolGS(A, b):
     R,Q = qr_gen(np.shape(A)[0], A)
-    Qt=np.transpose(Q)
+    Qt=transpose_np(Q)
     Rinv=np.linalg.inv(R)
-    return np.dot(np.dot(Rinv,Qt),b)
+    return np.dot(Rinv,np.dot(Qt,b))
+
+def resolDC(A,b):
+    L=DecompositionCholesky(A)
+    Lt=transpose_np(L)
+    Lt_inv, L_inv=np.linalg.inv(Lt), np.linalg.inv(L)
+    return np.dot(Lt_inv,np.dot(L_inv,b))
+
+def transpose_np(A):
+    B = np.zeros(shape=np.shape(A))
+    for i in range(np.shape(A)[0]):
+        B[:,i]=A[i,:]
+    return B
 
 #Exercice 3
 def compare(n):
     taille = [k for k in range(2, n+1)]
     TGS=[]
+    TDC=[]
     for e in taille:
         A=np.random.randint(1,100, size=(e,e))
         b=np.random.randint(1,100, size=(e,1))
         t1=time.time()
         resolGS(A, b)
         t2=time.time()
+        resolDC(A, b)
+        t3=time.time()
         TGS.append(t2-t1)
-    plt.plot(taille,TGS)
+        TDC.append(t3-t2)
+    plt.plot(taille,TGS,color='b')
+    plt.plot(taille,TDC,color='r')
     plt.show()
+
+def DecompositionCholesky(A):
+    n = np.shape(A)[0]
+    L = np.zeros(shape=(n,n))
+    for k in range(0,n):
+        for i in range(k,n):
+            if k == i:
+                somme=0
+                for j in range(0,k):
+                    somme += (L[k,j])**2
+                L[i,k]= (A[k,k]-somme)**(1/2)
+            else:
+                somme=0
+                for j in range(0,k):
+                    somme += L[k,j]*L[i,j]
+                L[i,k]=(A[i,k]-somme)/L[k,k]
+    return L
